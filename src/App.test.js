d85e5e82acaf14c2bdc46903ce2923fb3a15f334
Arzeezar <72872978+Arzeezar@users.noreply.github.com>
2021-04-05 16:37:09 +0700

    fix window.URL.createObjectURL is not a function

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.unmock("react-plotly.js");
+  if (typeof window.URL.createObjectURL === 'undefined') {
+  window.URL.createObjectURL = () => {
+    // Do nothing
+    // Mock this function for mapbox-gl to work
+  };
+}
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
