1e2d4acdcf76200c0903810cda82b155f36ccd18
Arzeezar Lording <72872978+Ar3ee3ar@users.noreply.github.com>
2021-04-22 00:12:18 +0700

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8080/api',
+    baseURL: 'https://ar3ee3ar.github.io/numer-react:8080/api',
 })
 
 export const getAllExam = () => api.get(`/exams`)
